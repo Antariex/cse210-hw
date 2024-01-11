b20bbf51b74b2f3b060aae760604302e955b6688
Ariel Antequiera <arielantequiera@gmail.com>
2024-01-11 00:01:08 -0300

    Add prep 1 exercise

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask the user for their name.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask the user for their lastname.
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Add a blank line.
+        Console.WriteLine();
+        
+        // Print the message to the user.
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
