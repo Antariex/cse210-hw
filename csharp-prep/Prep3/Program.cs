74b363ee018d6ff6b7a5e8bc97fcf92fd4703e05
Ariel Antequiera <arielantequiera@gmail.com>
2024-01-12 11:03:58 -0300

    Add prep 3 exercise

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int userGuess;
+            int attempts = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                userGuess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (magicNumber == userGuess)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You made {attempts} attempts to guess.");
+                    Console.WriteLine("");
+                    Console.Write("Do you want to play again? (yes/no): ");
+                    string userAnswer = Console.ReadLine();
+                    if (userAnswer.ToLower() == "no")
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Game Over.");
+                        return;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else if (magicNumber > userGuess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+            } while (magicNumber != userGuess);
+
+        } while (true);
     }
 }
