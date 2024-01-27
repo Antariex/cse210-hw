d707aee7cf0549fdc1f4139e08d090fb32e4021e
Ariel Antequiera <arielantequiera@gmail.com>
2024-01-26 22:08:18 -0300

    Add week 03 classes

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string userChoice;
+
+        if (userChoice == " ")
+        {
+            while (true)
+            {
+                Console.WriteLine("In the demo video, you can see that when the user pressed the enter key, words on the screen or were replaced by underscores. In reality, what happened is the console was cleared...");
+                Console.WriteLine("Press Enter to continue or type 'quit' to finish: ");
+                string userChoie = Console.ReadLine();
+            }
+
+        }
+    
+            else if (userChoice = "quit") { 
+    
+        Console.WriteLine("Finishing the program. Goodbye");
+                  
     }
-}
+}}
